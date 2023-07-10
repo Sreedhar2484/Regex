2182f9872746e9de9bc4dc191eab637b85c8a2c9
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-07-10 21:06:59 +0530

    Regex

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using RegexExample;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+
+    public static bool IsValidName(string name)
+    {
+        
+        string pattern = @"^[A-Za-z]{2,}$";
+
+        
+        Regex regex = new Regex(pattern);
+
+       
+        Match match = regex.Match(name);
+
+
+        return match.Success;
+    }
+    public static bool IsValidMobileNumber(string phoneNumber)
+    {
+        
+        string pattern = "^[6-9]{1}[0-9]{9}$";
+
+
+        Regex regex = new Regex(pattern);
+
+        
+        Match match = regex.Match(phoneNumber);
+
+        
+        return match.Success;
+    }
+    public static bool IsValidEmailAddress(string emailAddress)
+    {
+        
+        string pattern = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+
+        
+        Regex regex = new Regex(pattern);
+
+        
+        Match match = regex.Match(emailAddress);
+
+        
+        return match.Success;
+    }
+    public static bool IsValidPassword(string password)
+    {
+        
+        string pattern = @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=])[A-Za-z\d@#$%^&+=]{8,}$";
+
+
+        Regex regex = new Regex(pattern);
+
+        
+        Match match = regex.Match(password);
+
+        
+        return match.Success;
+    }
+
+
+    private static void Main(string[] args)
+
+
+
+    {
+
+
+        Console.WriteLine("Enter your first name:");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name:");
+        string lastName = Console.ReadLine();
+
+        if (IsValidName(firstName) && IsValidName(lastName))
+        {
+            Console.WriteLine("Valid first name and last name.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid first name or last name.");
+        }
+
+        Console.ReadLine();
+
+        Console.WriteLine("Enter a mobile number:");
+        string phoneNumber = Console.ReadLine();
+
+        Regex1 myMobileNum = new Regex1();
+
+        if (!IsValidMobileNumber(phoneNumber))
+        {
+            Console.WriteLine("Invalid mobile number.");
+        }
+        else
+        {
+            Console.WriteLine("Valid mobile number.");
+        }
+
+
+        Console.WriteLine("Enter an email address:");
+        string emailAddress = Console.ReadLine();
+
+        if (IsValidEmailAddress(emailAddress))
+        {
+            Console.WriteLine("Valid email address.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid email address.");
+        }
+
+        Console.ReadLine();
+
+
+        Console.WriteLine("Enter a password:");
+        string password = Console.ReadLine();
+
+        if (IsValidPassword(password))
+        {
+            Console.WriteLine("Valid password.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid password.");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ RegexExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExample
+{
+    internal class Regex1
+    {
+        public static bool IsValidMobileNumber(string phoneNumber)
+        {
+            // Regular expression pattern for a 10-digit mobile number
+            string pattern = @"^[0-9]{10}$";
+
+            // Create a Regex object with the pattern
+            Regex regex = new Regex(pattern);
+
+            // Use the Matches method to check if the phone number matches the pattern
+            Match match = regex.Match(phoneNumber);
+
+            // Return true if there is a match, false otherwise
+            return match.Success;
+        }
+
+    }
+}
